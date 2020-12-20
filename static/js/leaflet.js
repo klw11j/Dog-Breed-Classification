22d02677ff8a2f2f6df8925930a2b0dbcadf405f
Kelsey Wyatt <kwyattkkg@gmail.com>
2020-12-19 19:10:44 -0500

    adding animal shelter markers for each county

--- static/js/leaflet.js
+++ static/js/leaflet.js
@@ -12,20 +12,30 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(map);
 
+
 var shelter1 = L.marker([28.488800, -81.426758]).addTo(map);
-shelter1.bindPopup('<a href="http://www.orangecountyanimalservicesfl.net">Orange County Animal Services</a>' + "<br> 2769 Conroy Road, Orlando, FL 32839 <br>" + "407-836-3111 <br>")
+shelter1.bindPopup('<a href="http://www.orangecountyanimalservicesfl.net">Orange County Animal Services</a> <br>' + "<br> 2769 Conroy Road, Orlando, FL 32839 <br>" + "(407) 836-3111 <br>")
 
 var shelter2 = L.marker([28.570840, -81.287570]).addTo(map);
-shelter2.bindPopup('<a href="https://happytrailsanimalrescue.com">Happy Trails Animal Rescue</a>' + "<br> 1720 N Goldenrod Road #8, Orlando, FL 32807 <br>" + "407-270-7074 <br>")
-
-var shelter3 = L.marker([28.489180, -81.426850]).addTo(map);
-shelter3.bindPopup('<a href="https://petallianceorlando.org">Pet Alliance of Greater Orlando</a>' + "<br> 2727 Conroy Road, Orlando, FL 32839 <br>" + "407-351-7722 <br>")
+shelter2.bindPopup('<a href="https://happytrailsanimalrescue.com">Happy Trails Animal Rescue</a> <br>' + "<br> 1720 N Goldenrod Road #8, Orlando, FL 32807 <br>" + "(407) 270-7074 <br>")
 
 var shelter4 = L.marker([27.959260, -81.957490]).addTo(map);
-shelter4.bindPopup('<a href="https://www.spcaflorida.org">SPCA Florida Adoption Center</a>' + "<br> 5850 Brannen Road South, Lakeland, FL 33813 <br>" + "863-646-7722 <br>")
+shelter4.bindPopup('<a href="https://www.spcaflorida.org">SPCA Florida Adoption Center</a> <br>' + "<br> 5850 Brannen Road South, Lakeland, FL 33813 <br>" + "(863) 646-7722 <br>")
 
 var shelter5 = L.marker([28.39793, -80.70556]).addTo(map);
-shelter5.bindPopup('<a href="https://brevardhumanesociety.org/">Brevard Humane Society</a>' + "<br> 155 Pioneer Road, Merritt Island, FL 32953 <br>" + "321-636-3343 <br>")
+shelter5.bindPopup('<a href="https://brevardhumanesociety.org/">Brevard Humane Society</a> <br>' + "<br> 155 Pioneer Road, Merritt Island, FL 32953 <br>" + "(321) 636-3343 <br>")
 
-var shelter6 = L.marker([28.75660, -81.73622]).addTo(map);
-shelter6.bindPopup('<a href="https://lakecountyfl.gov/offices/animal_services/>Lake County Animal Shelter</a>' + "<br> 28123 County Rd 561, Tavares, FL 32778 <br>" + "352-343-9688 <br>")
+var shelter6 = L.marker([29.70437, -82.28168]).addTo(map);
+shelter6.bindPopup('<a href="https://alachuacounty.us/Depts/animalServices/Pages/AnimalServices.aspx">Alachua County Animal Services</a> <br>' + "<br> 3400 NE 53rd Avenue, Gainesville, FL 32609 <br>" + "(352) 264-6870 <br>")
+
+var shelter7 = L.marker([30.34321, -82.11362]).addTo(map);
+shelter7.bindPopup('<a href="http://www.bakercountyfl.org/animals.php">Baker County Animal Control</a> <br>' + "<br> 14564 Deputy Dawg Lane, Macclenny, FL 32063<br>" + "(904) 259-6786 <br>")
+
+var shelter8 = L.marker([30.17796, -85.64036]).addTo(map);
+shelter8.bindPopup('<a href="https://adoptme.org">The Humane Society of Bay County</a> <br>' + "<br> 1600 Bay Avenue, Panama City, FL 32405<br>" + "(850) 215-7120 <br>")
+
+var shelter9 = L.marker([29.96218, -82.17480]).addTo(map);
+shelter9.bindPopup('<a href="https://adoptme.org">Bradford County Animal Control</a> <br>' + "<br> 16946 SW 75th Avenue, Starke, FL 32091<br>" + "(904) 964-9200 <br>")
+
+var shelter10 = L.marker([29.96218, -82.17480]).addTo(map);
+shelter10.bindPopup('<a href="https://humanebroward.com">Humane Society of Broward County</a> <br>' + "<br> 2070 Griffin Road, Fort Lauderdale, FL 33312 <br>" + "(954) 989-3977 <br>")
